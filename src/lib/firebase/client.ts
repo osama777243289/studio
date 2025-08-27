6f3be54959ac3fc148716443c2ec1df0de5f6bb4
اسامة الخطيب <alkhteebosama211@gmail.com>
2025-08-27 00:31:43 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,23 +1,32 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCILuq1iVL_FNbmzvVzmtwEja0oKUePeEA",
+  apiKey: "AIzaSyDwbX9wZtlEmQjuFvkGe7iDofLGXWVvDaU",
   authDomain: "osama-2020.firebaseapp.com",
+  databaseURL: "https://osama-2020-default-rtdb.firebaseio.com",
   projectId: "osama-2020",
-  storageBucket: "osama-2020.appspot.com",
+  storageBucket: "osama-2020.firebasestorage.app",
   messagingSenderId: "390287828886",
-  appId: "1:390287828886:web:a12a3240c4974128f731e6"
+  appId: "1:390287828886:web:ce0523224f5de42c7cdfb7",
+  measurementId: "G-SW1C8RELTP"
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { app, db, auth };
+// Initialize Analytics and export it
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+export { app, db, auth, analytics };
